0162d87e06ae7358b803134598970927b49f5ccf
Tamicktom <rickpokemon38@gmail.com>
2025-05-06 01:18:57 -0300

    update variables.c to modify age and name variables

--- variables.c
+++ variables.c
@@ -8,6 +8,10 @@ int main()
 
   printf("There once was a man named %s\n", name);
   printf("He was %d years old\n", age);
+
+  age = 40; // Change age to 40
+  name[0] = 'M'; // Change name to "Mohn"
+
   printf("He really liked the name %s\n", name);
   printf("But didn't like being %d\n", age);
 
