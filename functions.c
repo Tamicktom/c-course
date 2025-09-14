34ee45563b78cb282357e71d4e512c3ae53a01cf
Tamicktom <rickpokemon38@gmail.com>
2025-09-14 14:36:27 -0300

    refactor functions.c to improve formatting and correct greeting message in sayHi function

--- functions.c
+++ functions.c
@@ -2,14 +2,14 @@
 
 void sayHi(char name[], int age);
 
-int main(){
+int main() {
 
   sayHi("John", 25);
 
   return 0;
 }
 
-void sayHi(char name[], int age){
+void sayHi(char name[], int age) {
   printf("Hellow %s, you are %d old", name, age);
 
   return;
