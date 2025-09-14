e87c84ae081681e7fd9ab0ef02d25e10bc7dcab6
Tamicktom <rickpokemon38@gmail.com>
2025-09-14 01:14:43 -0300

    refactor user_input.c to streamline input validation and enhance user interaction

---
+++ mad_lib.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  char color[20];
+  char pluralNoun[20];
+  char celebrity[20];
+
+  printf("Enter a color: ");
+  scanf("%s", color);
+
+  printf("Plural noun: ");
+  scanf("%s", pluralNoun);
+
+  printf("Celebrity: ");
+  scanf("%s", celebrity);
+
+  printf("Roses are %s\n", color);
+  printf("%s are blue\n", pluralNoun);
+  printf("I love %s\n", celebrity);
+
+  return 0;
+}
