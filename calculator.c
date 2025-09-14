440d2a420c042824bb6ef3eaf3202e5230ac35ab
Tamicktom <rickpokemon38@gmail.com>
2025-09-14 01:05:46 -0300

    refactor calculator.c to improve functionality by adding user input for two numbers and calculating their sum

--- calculator.c
+++ calculator.c
@@ -1,10 +1,18 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
   double num1, num2, result;
   
   printf("Enter first number: ");
-  
+  scanf("%lf", &num1);
 
+  printf("Enter the second number: ");
+  scanf("%lf", &num2);
+
+  result = num1 + num2;
+
+  printf("Result: %.2f", result);
+
+  return 0;
 }
